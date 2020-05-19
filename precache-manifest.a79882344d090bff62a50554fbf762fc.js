dfdd1dd14341b7b9496358d8f95dff164106e6ff
Andrew Weiss <wandrew8@gmail.com>
2020-05-19 14:08:33 -0700

    Updates

---
+++ precache-manifest.a79882344d090bff62a50554fbf762fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18267861c289ac0916629dcdaa1aae1a",
+    "url": "/characters-serach/index.html"
+  },
+  {
+    "revision": "52d4f9e93e03afd91c8c",
+    "url": "/characters-serach/static/css/main.02e52eed.chunk.css"
+  },
+  {
+    "revision": "cbf9ade6932bd7faf7fb",
+    "url": "/characters-serach/static/js/2.a27a171a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/characters-serach/static/js/2.a27a171a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52d4f9e93e03afd91c8c",
+    "url": "/characters-serach/static/js/main.907b4b93.chunk.js"
+  },
+  {
+    "revision": "18c17175716952b3d49f",
+    "url": "/characters-serach/static/js/runtime-main.b16c50b8.js"
+  }
+]);
